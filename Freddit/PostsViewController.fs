072bfc2737ac89ce395e67a435b0b057f46dab40
Andrew Carter <andrew.carter@willowtreeapps.com>
2017-06-02 16:16:52 -0400

    Can now view a reddit post!

--- Freddit/PostsViewController.fs
+++ Freddit/PostsViewController.fs
@@ -4,6 +4,7 @@ open FSharp.Data
 open System
 open Foundation
 open UIKit
+open SafariServices
 open Result
 
 [<Register ("PostsViewController")>]
@@ -15,16 +16,19 @@ type PostsViewController (handle:IntPtr) as this =
     let tableViewDelegate = new PostsTableViewDelegate ()
     let tableViewDataSource = new PostsTableViewDataSource ()
 
-    let postSelected (tableView:UITableView) (indexPath:NSIndexPath) =
-        printfn "selected"
-
     [<Outlet>]
     member val TableView = null :> UITableView with get, set
+
+    member this.PostSelected (tableView:UITableView) (indexPath:NSIndexPath) =
+        let post = tableViewDataSource.Posts.[indexPath.Row]
+        let url = NSUrl.FromString post.Data.Url
+        let controller = new SFSafariViewController (url)
+        this.NavigationController.PushViewController (controller, true)
 
     override this.ViewDidLoad () =
         base.ViewDidLoad ()
         this.setupTableView
-        tableViewDelegate.RowSelectionDelegate <- Some <| PostsTableViewDelegateRowSelectionDelegate postSelected
+        tableViewDelegate.RowSelectionDelegate <- Some <| PostsTableViewDelegateRowSelectionDelegate this.PostSelected
 
     override this.ViewDidAppear (animated:bool) =
         base.ViewDidAppear (animated)
